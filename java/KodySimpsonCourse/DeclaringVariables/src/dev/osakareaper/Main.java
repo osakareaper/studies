1bf4eaa664e75d42dc11ffaad99a42ee1de39498
osakareaper <osakareaper@gmail.com>
2024-01-27 01:25:19 -0300

    studies

---
+++ java/KodySimpsonCourse/DeclaringVariables/src/dev/osakareaper/Main.java
@@ -0,0 +1,25 @@
+package dev.osakareaper;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Declaring Variables
+        // [type] [identifier] = [data];
+
+        // Integers
+        int brainSize = 99999;
+        System.out.println("My brain size: " + brainSize);
+
+        // Floating points
+        double testScore = 9.5;
+        System.out.println("My 1st test score: " + testScore);
+
+        float testScore2 = 8.7F;
+        System.out.println("My 2nd test score: " + testScore2);
+
+        // String
+        String toPuto = "DOESN'T EVEN MAKE SENSE WTFFFFF";
+        System.out.println(toPuto);
+
+    }
+}
---
+++ java/KodySimpsonCourse/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    // entry point
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(1+33);
+        System.out.println("The amount of people that i have killed: " + 69);
+    }
+
+}
